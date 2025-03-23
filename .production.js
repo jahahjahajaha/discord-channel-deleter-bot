6c014454e8192fef318fa49ddcd67c967d1b6ecc
KnarliX <rajnish993123@gmail.com>
2025-03-23 18:25:48 +0000

    .

--- .production.js
+++
@@ -1,7 +0,0 @@
-// This file is used by Render.com when deploying the application in production mode
-// It loads the compiled server code directly instead of using Vite
-
-import('./dist/index.js').catch(err => {
-  console.error('❌ Failed to start production server:', err);
-  process.exit(1);
-});
